d859280a990c9a6f8843ef166496c5facaf323ab
Joe Quattrone <joequattrone5@gmail.com>
2018-11-08 16:37:48 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -109,5 +109,16 @@ class TicTacToe
       @board[won?[0]]
     end
   end
+  
+  def play
+  until over? 
+   turn
+  end
+   if won?
+     puts "Congratulations #{winner}!"
+   else
+     puts "Cat's Game!"
+  end
+end
 
 end
