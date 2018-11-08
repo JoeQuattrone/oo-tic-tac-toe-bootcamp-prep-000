42e275f9b0295e45a65c8f776a1fdcfafddf65e4
Joe Quattrone <joequattrone5@gmail.com>
2018-11-08 16:10:32 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,20 @@
 class TicTacToe
+  
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
+  end
+    
+  def win_combinations
+    @WIN_COMBINATIONS = [
+  [0,1,2], #Top row
+  [3,4,5], #Middle row
+  [6,7,8], #Bottom row
+  [0,3,6], #Left-vert row
+  [1,4,7], #Middle-vert row
+  [2,5,8], #Right-vert row
+  [0,4,8], #Left-diagnol row
+  [2,4,6] #Right-diagnol row
+  ]
   end
     
 end
